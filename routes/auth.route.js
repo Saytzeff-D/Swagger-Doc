f7e2a295f281a962385dbea58529e4c7f2ee51f3
David Atanda <ololadedavid15@gmail.com>
2023-08-01 16:40:53 +0100

    get route for currentuser

--- routes/auth.route.js
+++ routes/auth.route.js
@@ -1,8 +1,10 @@
 const express = require('express')
-const { login, register } = require('../controllers/auth.controller')
+const { login, register, currentUser } = require('../controllers/auth.controller')
+const { authJWT } = require('../middlewares/jwt.middleware')
 const AuthRouter = express.Router()
 
 AuthRouter.post('/login', login)
 AuthRouter.post('/register', register)
+AuthRouter.get('/currentUser', authJWT, currentUser)
 
 module.exports = AuthRouter

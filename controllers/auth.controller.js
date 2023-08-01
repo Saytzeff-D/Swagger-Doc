e0410ccc8239f7751b1329256106c0281ecadc95
Vida-TG <victorademiju.t@gmail.com>
2023-08-01 02:14:08 +0100

    Added bcrypt

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -1,30 +1,35 @@
 const pool = require("../connections/pool")
 const jwt = require('jsonwebtoken')
+const bcrypt = require('bcrypt')
 
-const register = (req, res)=>{
+const register = async (req, res) =>{
     let payload = req.body
-    const sql = `INSERT INTO users (email, password) VALUES('${payload.email}', '${payload.password}')`
+    const hashedPassword = await bcrypt.hash(payload.password, 10);
+    const sql = `INSERT INTO users (email, password) VALUES('${payload.email}', '${hashedPassword}')`
     pool.query(sql, (err, result)=>{
         if (!err) {
             res.status(200).json({message: 'Success'})
         } else {
-            res.status(300).json({message: 'Internal Server Error'})
+            res.status(500).json({message: 'Internal Server Error'})
         }
     })
 }
-const login = (req, res)=>{
+const login = (req, res) => {
     let payload = req.body
     const checkEmail = `SELECT * FROM users WHERE email = '${payload.email}'`
-    pool.query(checkEmail, (err, result)=>{
+    pool.query(checkEmail, async (err, result)=>{
         const user = result
         if (err) {
-            res.status(300).json({message: 'Internal Server Error'})
+            return res.status(500).json({message: 'Internal Server Error'})
+        }
+        if (user.length == 0) {
+            return res.status(200).json({message: 'User not found'})
+        }
+        if (await bcrypt.compare(user[0].password, payload.password)) {
+            accessToken(user)
+            res.send('Logged in!')
         } else {
-            if (user.length == 0) {
-                res.status(200).json({message: 'User not found'})
-            } else {
-                user[0].password == payload.password ? accessToken(user) : res.status(200).json({message: 'Incorrect Password'})
-            }
+            return res.status(200).json({message: 'Incorrect Password'})
         }
     })
 }

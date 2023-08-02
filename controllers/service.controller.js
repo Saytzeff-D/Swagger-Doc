3fac95747eda7e282dc08596162f064c1bafb8ec
Vida-TG <victorademiju.t@gmail.com>
2023-08-02 14:18:54 +0100

    Create service route

--- controllers/service.controller.js
+++ controllers/service.controller.js
@@ -54,4 +54,38 @@ const getService = (req, res) => {
 };
 
 
-module.exports = { allCountryServices, getService }
+
+const createService = (req, res) => {
+    const { name, description, image, price, country_id, category } = req.body;
+
+    const serviceExistsQuery = 'SELECT COUNT(*) AS count FROM services WHERE name = ? AND country_id = ?';
+    const serviceExistsValues = [name, country_id];
+
+    pool.query(serviceExistsQuery, serviceExistsValues, (err, result) => {
+        if (err) {
+            console.error('Error checking if service exists:', err);
+            return res.status(500).json({ message: 'Internal Server Error' });
+        }
+  
+        const serviceExists = result[0].count > 0;
+  
+        if (serviceExists) {
+            return res.status(409).json({ message: 'Service with the same name and country already exists' });
+        }
+  
+        const createServiceSql = 'INSERT INTO services (name, description, image, price, country_id, category) VALUES (?, ?, ?, ?, ?, ?)';
+        const createServiceValues = [name, description, image, price, country_id, category];
+  
+        pool.query(createServiceSql, createServiceValues, (err, result) => {
+            if (err) {
+                console.error('Error creating a new service:', err);
+                return res.status(500).json({ message: 'Internal Server Error' });
+            }
+            return res.status(201).json({ message: 'Service created successfully', serviceId: result.insertId });
+        });
+    });
+};
+  
+
+
+module.exports = { allCountryServices, getService, createService }
--- routes/serviceRoute.js
+++ routes/serviceRoute.js
@@ -1,7 +1,9 @@
 const ServiceRouter = require('express').Router()
-const { getService, allCountryServices } = require('../controllers/service.controller')
+const { isAdmin, isAuth } = require('../middlewares/authMiddleware')
+const { getService, allCountryServices, createService } = require('../controllers/service.controller')
 
-ServiceRouter.post('/:serviceId', getService)
+ServiceRouter.post('/', isAuth, isAdmin, createService)
+ServiceRouter.get('/:serviceId', getService)
 ServiceRouter.get('/country/:countryId', allCountryServices)
 
 module.exports = ServiceRouter

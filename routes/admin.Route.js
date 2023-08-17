fd0162ada5588efca2ae51764279509e28454b30
Vida-TG <victorademiju.t@gmail.com>
2023-08-18 00:59:14 +0100

    Admin routes

--- routes/admin.Route.js
+++ routes/admin.Route.js
@@ -1,9 +1,10 @@
 const AdminRouter = require("express").Router()
 const { isAuth, isAdmin } = require("../middlewares/auth.middleware")
-const { allNotifications } = require("../controllers/notification.controller")
+const { allNotifications, getAllUsers, deleteUser } = require("../controllers/admin.controller")
 
+AdminRouter.get('/users', getAllUsers);
+AdminRouter.delete('user/:userId', deleteUser)
 AdminRouter.get('/notifications', allNotifications);
-
 
 
 module.exports = AdminRouter

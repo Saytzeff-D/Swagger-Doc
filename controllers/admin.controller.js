7edeeba6e33b4b7ab675ae330b658ccfe907c1a1
Vida-TG <victorademiju.t@gmail.com>
2023-08-18 10:22:20 +0100

    Specific notifications

--- controllers/admin.controller.js
+++ controllers/admin.controller.js
@@ -28,4 +28,101 @@ const allNotifications = (req, res) => {
 };
 
 
-module.exports = { allNotifications };
+
+const getAllUsers = (req, res) => {
+    const sql = `
+        SELECT id, email, firstname, lastname, username, isAdmin
+        FROM users
+    `;
+    
+    pool.query(sql, (err, result) => {
+        if (err) {
+            return res.status(500).json({ message: 'Internal Server Error', err });
+        }
+
+        return res.status(200).json({ users: result });
+    });
+};
+
+
+
+const deleteUser = (req, res) => {
+    const userId = req.params.userId;
+
+    const sql = `
+        DELETE FROM users
+        WHERE id = ?
+    `;
+
+    pool.query(sql, [userId], (err, result) => {
+        if (err) {
+            return res.status(500).json({ message: 'Internal Server Error', err });
+        }
+
+        return res.status(200).json({ message: 'User deleted successfully' });
+    });
+};
+
+
+
+const getTransactionNotifications = (req, res) => {
+    const sql = `
+        SELECT n.id, n.type, n.read, n.date, t.service_id, t.user_id
+        FROM notifications n
+        JOIN transactions t ON n.transaction_id = t.id
+        WHERE n.transaction_id IS NOT NULL AND n.document_id IS NULL AND n.user_id IS NULL
+    `;
+    
+    pool.query(sql, (err, result) => {
+        if (err) {
+            return res.status(500).json({ message: 'Internal Server Error', err });
+        }
+
+        return res.status(200).json({ notifications: result });
+    });
+};
+
+
+const getDocumentNotifications = (req, res) => {
+    const sql = `
+        SELECT n.id, n.type, n.read, n.date, d.service_id, d.user_id
+        FROM notifications n
+        JOIN documents d ON n.document_id = d.id
+        WHERE n.document_id IS NOT NULL AND n.transaction_id IS NULL AND n.user_id IS NULL
+    `;
+    
+    pool.query(sql, (err, result) => {
+        if (err) {
+            return res.status(500).json({ message: 'Internal Server Error', err });
+        }
+
+        return res.status(200).json({ notifications: result });
+    });
+};
+
+
+const getNewUserNotifications = (req, res) => {
+    const sql = `
+        SELECT n.id, n.type, n.read, n.date, u.id AS user_id
+        FROM notifications n
+        JOIN users u ON n.user_id = u.id
+        WHERE n.user_id IS NOT NULL AND n.transaction_id IS NULL AND n.document_id IS NULL
+    `;
+    
+    pool.query(sql, (err, result) => {
+        if (err) {
+            return res.status(500).json({ message: 'Internal Server Error', err });
+        }
+
+        return res.status(200).json({ notifications: result });
+    });
+};
+
+
+
+
+
+
+
+
+module.exports = { getAllUsers, deleteUser, allNotifications, getTransactionNotifications, getDocumentNotifications, getNewUserNotifications };
--- routes/admin.Route.js
+++ routes/admin.Route.js
@@ -1,10 +1,13 @@
 const AdminRouter = require("express").Router()
 const { isAuth, isAdmin } = require("../middlewares/auth.middleware")
-const { allNotifications, getAllUsers, deleteUser } = require("../controllers/admin.controller")
+const { allNotifications, getAllUsers, deleteUser, getTransactionNotifications, getNewUserNotifications, getDocumentNotifications } = require("../controllers/admin.controller")
 
 AdminRouter.get('/users', getAllUsers);
-AdminRouter.delete('user/:userId', deleteUser)
+AdminRouter.delete('user/:userId', deleteUser);
 AdminRouter.get('/notifications', allNotifications);
+AdminRouter.get('/notifications/transactions', getTransactionNotifications);
+AdminRouter.get('/notifications/users', getNewUserNotifications);
+AdminRouter.get('/notifications/documents', getDocumentNotifications);
 
 
 module.exports = AdminRouter

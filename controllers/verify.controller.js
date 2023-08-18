94491e5965e60ba6aa82972ebc6b11df368fd453
David Atanda <ololadedavid15@gmail.com>
2023-08-18 15:43:17 +0100

    now sending verification code to user's emails

--- controllers/verify.controller.js
+++ controllers/verify.controller.js
@@ -1,4 +1,5 @@
-const twilio = require('twilio')
+const twilio = require('twilio');
+const { transporter, mailOption } = require('../mailer');
 
 const sendSMS = (req, res)=>{
     const accountSid = process.env.TWILIO_ACCOUNT_SID;
@@ -12,5 +13,17 @@ const sendSMS = (req, res)=>{
             res.json({message: 'Sent', verify: verification.sid})
         });
 }
+const verifyEmail = (req, res)=>{
+    const email = req.body.email 
+    let verificationCode = Math.floor(Math.random()*1000000)
+    transporter.sendMail(mailOption(verificationCode, email), (err, info)=>{
+        if(err){
+            console.log(err)
+            res.status(500).json({status: false, message: 'Fail to send code', email})
+        }else{
+            res.status(200).json({status: true, message: 'Success', email, info})
+        }
+    })
+}
 
-module.exports = { sendSMS }
+module.exports = { sendSMS, verifyEmail }
--- routes/verify.route.js
+++ routes/verify.route.js
@@ -1,7 +1,8 @@
 const express = require('express')
-const { sendSMS } = require('../controllers/verify.controller')
+const { sendSMS, verifyEmail } = require('../controllers/verify.controller')
 const VerifyRouter = express.Router()
 
 VerifyRouter.get('/sms', sendSMS)
+VerifyRouter.post('/email', verifyEmail)
 
 module.exports =  VerifyRouter

9f3b731be36e432c35c656eb59cbe45682fc7d8d
David Atanda <ololadedavid15@gmail.com>
2023-08-21 00:44:15 +0100

    now fetching user email

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -10,20 +10,22 @@ const register = async (req, res) => {
     const checkEmailQuery = `SELECT COUNT(*) AS count FROM users WHERE email = ?`
     pool.query(checkEmailQuery, [payload.email], async (err, result) => {
         const emailExists = result[0].count > 0;
-        if (emailExists) {
-            return res.status(409).json({ message: 'This mail has previously been registered' });
+        if (emailExists) {            
+            return res.status(200).json({ message: 'This mail has previously been registered' });
+        }else {
+            const values = [payload.email, hashedPassword, payload.phonenum]
+            const sql = `INSERT INTO users (email, password, phonenum) VALUES(?, ?, ?)`
+            pool.query(sql, values, (err, result) => {
+                if (!err) {
+                    sendVerificationCode(res, payload.email)
+                    // res.status(200).json({message: 'Success'})
+                } else {
+                    console.log(err)
+                    res.status(500).json({message: 'Internal Server Error'})
+                }
+            })
         }
-    })
-    const values = [payload.email, hashedPassword, payload.phonenum]
-    const sql = `INSERT INTO users (email, password, phonenum) VALUES(?, ?, ?)`
-    pool.query(sql, values, (err, result) => {
-        if (!err) {
-            res.status(200).json({message: 'Success'})
-        } else {
-            console.log(err)
-            res.status(500).json({message: 'Internal Server Error'})
-        }
-    })
+    })    
 }
 
 
@@ -35,20 +37,21 @@ const login = (req, res) => {
         const user = result
         if (err) {
             return res.status(500).json({message: 'Internal Server Error'})
-        }
-        if (user.length == 0) {
-            return res.status(200).json({status: false, message: 'User not found'})
-        }
-        if (await bcrypt.compare(payload.password, user[0].password)) {
-            if (user.is_phone_verified) {
-                const token = accessToken(user)
-                res.status(200).json({status: true, token})
-            } else {
-                sendVerificationCode(res, user);
+        }else {
+            if (user.length == 0) {
+                return res.status(200).json({status: false, message: 'User not found'})
             }
-        } else {
-            return res.status(200).json({status: false, message: 'Incorrect Password'})
-        }
+            else if (await bcrypt.compare(payload.password, user[0].password)) {
+                if (user.is_phone_verified) {
+                    const token = accessToken(user)
+                    res.status(200).json({status: true, token, verify: true})
+                } else {
+                    sendVerificationCode(res, payload.email);
+                }
+            } else {
+                return res.status(200).json({status: false, message: 'Incorrect Password'})
+            }
+        }        
     })
 }
 

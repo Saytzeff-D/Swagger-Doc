d3738007a2a81f4f0a9a9a26f2ac80452cc159e1
David Atanda <ololadedavid15@gmail.com>
2023-08-22 18:42:18 +0100

    choose chapter route

--- routes/chapter.route.js
+++ routes/chapter.route.js
@@ -1,12 +1,13 @@
 const ChapterRouter = require("express").Router()
-const { isAuth, isAdmin } = require("../middlewares/auth.middleware")
-const { updateChapter, deleteChapter, createChapter, getChapter, getChapters } = require("../controllers/chapter.controller")
+const { isAuth, isAdmin, authJWT } = require("../middlewares/auth.middleware")
+const { updateChapter, deleteChapter, createChapter, getChapter, getChapters, chooseChapter } = require("../controllers/chapter.controller")
 
 ChapterRouter.get('/chapters', getChapters);
 ChapterRouter.get('/chapters/:id', getChapter);
-ChapterRouter.post('/chapters', isAuth, isAdmin, createChapter);
-ChapterRouter.put('/chapters/:id', isAuth, isAdmin, updateChapter);
-ChapterRouter.delete('/chapters/:id', isAuth, isAdmin, deleteChapter);
+ChapterRouter.post('/chapters', createChapter);
+ChapterRouter.put('/chapters/:id', updateChapter);
+ChapterRouter.delete('/chapters/:id', deleteChapter);
+ChapterRouter.put('/choose', authJWT, chooseChapter)
 
 
 

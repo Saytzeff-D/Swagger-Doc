d66af747a387b302d17b8c9702848ca305792467
David Atanda <ololadedavid15@gmail.com>
2023-08-22 18:42:07 +0100

    now choosing chapter

--- controllers/chapter.controller.js
+++ controllers/chapter.controller.js
@@ -9,12 +9,9 @@ const getChapters = (req, res) => {
         console.error('Error fetching chapters:', err);
         return res.status(500).send({ message: 'Internal Server Error' });
       }
-
-      return res.status(200).send(result);
+      return res.status(200).send({chapters: result, chapterCount: result.length});      
     });
 };
-
-
 const getChapter = (req, res) => {
     const values = [req.params.id]
     const sql = `SELECT * FROM chapters WHERE id = ?`
@@ -24,42 +21,45 @@ const getChapter = (req, res) => {
         console.error('Error fetching chapter:', err);
         return res.status(500).send({ message: 'Internal Server Error' });
       }
-
       return res.status(200).send(result);
+      
     });
 };
-
-
-
-
+const chooseChapter = (req, res)=>{
+    const payload = req.body
+    console.log(payload, req.user)
+    const sql = `UPDATE users SET chapter_id = ? WHERE id = ?`
+    pool.query(sql, [payload.chapter_id, req.user.id], (err, result)=>{
+        if (!err) {
+            res.status(200).json({message: 'Success'})
+        } else {
+            res.status(500).json({message: 'Internal Server Error'})
+        }
+    })
+}
 const createChapter = (req, res) => {
     const { name, image, description } = req.body;
     const values = [ name, image, description ];
     const chapterExistsQuery = `SELECT COUNT(*) AS count FROM chapters WHERE name = ?`;
     pool.query(chapterExistsQuery, [ name ], (err, result) => {
-        if (err) {
+        if (err) {            
             return res.status(500).json({ message: 'Internal Server Error' });
         }
   
         const chapterExists = result[0].count > 0;
         if (chapterExists) {
-            return res.status(409).json({ message: 'Chapter with the same name already exists' });
+            return res.status(201).json({ status: false, message: 'Chapter with the same name already exists' });
         }
         const sql = `INSERT INTO chapters (name, image, description) VALUES (?, ?, ?)`;
         pool.query(sql, values, (err, result) => {
-            if (err) {
-            console.error('Error creating a new chapter:', err);
+            if (err) {    
+                console.log(err)        
             return res.status(500).json({ message: 'Internal Server Error' });
             }
-            return res.status(201).json({ message: 'Chapter created successfully', chapterId: result.insertId });
+            return res.status(200).json({ status: true, message: 'Chapter created successfully', chapterId: result.insertId });
         });
     });
 };
-
-  
-
-
-
 const updateChapter = (req, res) => {
     const { name, image, description } = req.body;
     const values = [ name, image, description, req.params.id ];
@@ -77,10 +77,6 @@ const updateChapter = (req, res) => {
         return res.status(200).json({ message: 'Chapter updated successfully' });
     });
 };
-  
-
-
-
 const deleteChapter = (req, res) => {
     const sql = `DELETE FROM chapters WHERE id = ?`;
     pool.query(sql, [ req.params.id ], (err, result) => {
@@ -89,11 +85,11 @@ const deleteChapter = (req, res) => {
             return res.status(500).json({ message: 'Internal Server Error' });
         }
         if (result.affectedRows === 0) {
-            return res.status(404).json({ message: 'Chapter not found' });
+            return res.status(200).json({ status: false, message: 'Chapter not found' });
         }
-        return res.status(200).json({ message: 'Chapter deleted successfully' });
+        return res.status(200).json({ status: true, message: 'Chapter deleted successfully' });
     });
 };
   
 
-module.exports = { getChapters, createChapter, getChapter, updateChapter, deleteChapter }
+module.exports = { getChapters, createChapter, getChapter, updateChapter, deleteChapter, chooseChapter }

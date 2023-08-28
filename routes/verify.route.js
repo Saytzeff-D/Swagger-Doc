b5c2db9d12eec5074fb38d0aa38cbcb63c8b991a
David Atanda <ololadedavid15@gmail.com>
2023-08-28 20:27:01 +0100

    modified the controllers

--- routes/verify.route.js
+++ routes/verify.route.js
@@ -1,11 +1,12 @@
 const express = require('express')
-const { verifySmsCode, sendPasswordResetCode, verifyEmailCodeForReset, resetPassword } = require('../controllers/verify.controller')
 const { resetToken } = require('../middlewares/reset.middleware')
+const { verifySmsCode } = require('../controllers/sms.controller')
+const { sendOtpCode, verifyOtpCode, resetPassword } = require('../controllers/reset-password.controller')
 const VerifyRouter = express.Router()
 
 VerifyRouter.post('/sms/:userId', verifySmsCode)
-VerifyRouter.post('/forgot-password', sendPasswordResetCode)
-VerifyRouter.post('/verify-reset-code', verifyEmailCodeForReset)
+VerifyRouter.post('/forgot-password', sendOtpCode)
+VerifyRouter.post('/verify-otp-code', verifyOtpCode)
 VerifyRouter.put('/reset-password', resetToken, resetPassword)
 
 module.exports =  VerifyRouter

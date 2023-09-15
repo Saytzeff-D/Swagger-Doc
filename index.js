1d629e9b8e8045ca1b3f6fbaa17b07b9174141ec
David Atanda <ololadedavid15@gmail.com>
2023-09-15 11:07:13 +0100

    working on swagger ui

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ require('dotenv').config()
 const cors = require('cors');
 const bodyParser = require("body-parser");
 const pool = require('./connections/pool');
+const swaggerUI = require('swagger-ui-express')
+const swaggerDocs = require('swagger-jsdoc')
 const AuthRouter = require('./routes/auth.route');
 const AdminRouter = require('./routes/admin.route');
 const SeedRouter = require('./routes/seed.route');
@@ -11,9 +13,30 @@ const ChapterRouter = require('./routes/chapter.route');
 const DocumentRouter = require('./routes/documents.route');
 const VerifyRouter = require('./routes/verify.route');
 
+const options = {
+    definition: {
+        openapi: "3.0.0",
+        info: {
+            title: 'ForeignWR API DOCS',
+            version: '0.0.1',
+            description: 'Rest API for Foreign Wives Reign'
+        }
+    },
+    servers: [
+        {
+            url: 'http://localhost:5050/'
+        }
+    ],
+    apis: [
+        './controllers/*.js'
+    ]
+}
+
+const specs = swaggerDocs(options)
 app.use(bodyParser.urlencoded({extended: true}))
 app.use(bodyParser.json())
 app.use(cors({origin: '*'}))
+app.use('/api', swaggerUI.serve, swaggerUI.setup(specs))
 app.use('/auth', AuthRouter)
 app.use('/admin', AdminRouter)
 app.use('/chapter', ChapterRouter)
